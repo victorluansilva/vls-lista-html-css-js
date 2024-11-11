afe8f8ef571487b44e6b49ab8d6bce45adb6acc9
Victor Silva <49527666+victorluansilva@users.noreply.github.com>
2024-11-11 20:22:41 -0300

    Exemplos referentes à aula de POO

---
+++ POO/paradigmaPOO.js
@@ -0,0 +1,12 @@
+import Pessoa from "./classes/pessoa.js";
+import Carro from "./classes/carro.js";
+let minhaPessoa = new Pessoa("Victor", 29);
+let meuCarro = new Carro(minhaPessoa, "Vermelho", 2024, "Chevrollet","Ônix",13,"etanol")
+
+console.log(minhaPessoa);
+
+console.log(minhaPessoa.nome);
+console.log(minhaPessoa.idade);
+
+minhaPessoa.falar();
+console.log(meuCarro.info());
