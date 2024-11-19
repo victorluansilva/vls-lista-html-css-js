75fdc566d882879e3e1d3b709e55f811f5765be0
victorluansilva <victorluansilva@gmail.com>
2024-11-19 20:09:55 -0300

    Estilização base para o script

--- JAVASCRIPT/ARRAYS/index.js
+++ JAVASCRIPT/ARRAYS/index.js
@@ -1,5 +1,43 @@
-let nomes = [];
+let grupos = [];
+let nomes = ["Cauã", "Dayane", "Leo Stronda", "Japoneso", "Renato", "Jhony Due", "Igor igo igo"];
+
+grupos.push(nomes)
+grupos.push(nomes)
+grupos.push(nomes)
 
 
+const grupoNomes = (id, grupo) => {
+    const liUlNomes = () =>{
+        let list = ``
+        grupo.forEach(nome => {
+            list += `<li>${nome}</li>`
+        })
+        return list;
+    }
 
-console.log(nomes);
+    return `<div id='${id}' class="grupoNomes">
+                <h2>Grupo ${(id+1)}</h2>
+                <hr>
+                <ul>
+                    ${liUlNomes()}
+                </ul>
+            </div>
+        `
+}
+
+
+console.log(grupos);
+
+
+const btnGerar = document.getElementById('btnGerar');
+const divResultado = document.getElementById('resultado');
+
+btnGerar.addEventListener('click', () => {
+    let saidaFinal = ``;
+    grupos.forEach((grupo,index) =>{
+        saidaFinal += grupoNomes(index, grupo);
+    });
+
+    divResultado.innerHTML = saidaFinal;
+
+});
