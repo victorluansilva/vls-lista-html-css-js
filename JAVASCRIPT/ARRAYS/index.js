762f5c2300754ab37a4d11994bfb6b76706fdc06
victorluansilva <victorluansilva@gmail.com>
2024-11-21 19:14:23 -0300

    Sorteador de nomes em grupos

--- JAVASCRIPT/ARRAYS/index.js
+++ JAVASCRIPT/ARRAYS/index.js
@@ -3,10 +3,10 @@ const qtdGrupos = document.getElementById('qtdGrupos');
 const btnGerar = document.getElementById('btnGerar');
 const divResultado = document.getElementById('resultado');
 
-const valorInicial = () => { 
-const nomes = ["Cauã", "Dayane", "Leo Stronda", "Japoneso", "Renato", "Jhony Due", "Igor igo igo", "Guinaldo", "Gordão", "Carlão"];
-textArea.value = nomes.join(', \n');
-qtdGrupos.value = 2;
+const valorInicial = () => {
+    const nomes = ["Cauã", "Dayane", "Leo Stronda", "Japoneso", "Renato", "Jhony Due", "Igor igo igo", "Guinaldo", "Gordão", "Carlão"];
+    textArea.value = nomes.join(', \n');
+    qtdGrupos.value = 2;
 }
 
 window.onload = () => {
@@ -15,17 +15,19 @@ window.onload = () => {
 
 btnGerar.addEventListener('click', () => {
 
+    divResultado.innerHTML = " ";
+
     const grupoNomes = (id, grupo) => {
-        const liUlNomes = () =>{
+        const liUlNomes = () => {
             let list = ``
             grupo.forEach(nome => {
                 list += `<li>${nome}</li>`
             })
             return list;
         }
-    
+
         return `<div id='gp-${id}' class="grupoNomes">
-                    <h2>Grupo ${(id+1)}</h2>
+                    <h2>Grupo ${(id + 1)}</h2>
                     <hr>
                     <ul>
                         ${liUlNomes()}
@@ -34,7 +36,14 @@ btnGerar.addEventListener('click', () => {
             `
     }
 
-    
+    const nomes = textArea.value.split(',').map(nome => nome.trim()).filter(nome => nome);
+
+    const grupos = Array.from({length: qtdGrupos.value}, () => []);
+    const nomeSorteados = nomes.sort(() => Math.random() - 0.5)
+
+    nomeSorteados.forEach((nome,index) =>{
+        grupos[index % qtdGrupos.value].push(nome);
+    });    
 
     let saidaFinal = ``;
 
